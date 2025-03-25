f8a24b4a7049bf97027f093312472378fadd5969
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 15:17:46 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,19 +3,19 @@ class Database {
     // Set private variable to hold the connection
     private $conn;
 
-    // Database connection parameters
-    private $host = 'localhost';
-    private $db_name = 'quotesdb';
-    private $username = 'root';
-    private $password = '';
+    // Database connection parameters using Render's provided credentials
+    private $host = 'dpg-cvgtcqqqgecs73clgg2g-a'; // Internal hostname for Render services
+    private $db_name = 'inf653_midtermproj'; // Database name
+    private $username = 'inf653_midtermproj_user'; // Username
+    private $password = 'SU5971hNKDk2IeVbBBfz09jsCdqETpSg'; // Password
 
     // Get the database connection
     public function connect() {
         $this->conn = null;
 
         try {
-            // Prepare the DSN (Data Source Name)
-            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name . ';charset=utf8mb4';
+            // Prepare the DSN (Data Source Name) for PostgreSQL
+            $dsn = 'pgsql:host=' . $this->host . ';port=5432;dbname=' . $this->db_name . ';options=\'--client_encoding=UTF8\'';
 
             // Create a new PDO connection
             $this->conn = new PDO($dsn, $this->username, $this->password);
