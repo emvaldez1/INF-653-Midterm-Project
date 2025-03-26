da32588dccf979bf21e790fd4aa29776813bbd61
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 20:48:00 -0500

    Update update.php

--- api/categories/update.php
+++ api/categories/update.php
@@ -5,30 +5,26 @@ header('Access-Control-Allow-Methods: PUT');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once '../../config/Database.php';
-include_once '../../models/Quote.php';
+include_once '../../models/Category.php';
 
 $database = new Database();
 $db = $database->connect();
 
-$quote = new Quote($db);
+$category = new Category($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
-if (!empty($data->id) && !empty($data->quote) && !empty($data->author_id) && !empty($data->category_id)) {
-    $quote->id = $data->id;
-    $quote->quote = $data->quote;
-    $quote->author_id = $data->author_id;
-    $quote->category_id = $data->category_id;
+if (!empty($data->id) && !empty($data->category)) {
+    $category->id = $data->id;
+    $category->category = $data->category;
 
-    if ($quote->update()) {
+    if ($category->update()) {
         echo json_encode([
-            'id' => $quote->id,
-            'quote' => $quote->quote,
-            'author_id' => $quote->author_id,
-            'category_id' => $quote->category_id
+            'id' => $category->id,
+            'category' => $category->category
         ]);
     } else {
-        echo json_encode(['message' => 'No Quotes Found']);
+        echo json_encode(['message' => 'No Categories Found']);
     }
 } else {
     echo json_encode(['message' => 'Missing Required Parameters']);
