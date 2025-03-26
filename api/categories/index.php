5065d439e7c601e2eff0745414080d9c45f5c9d1
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 20:37:49 -0500

    Update index.php

--- api/categories/index.php
+++ api/categories/index.php
@@ -1,49 +1,66 @@
 <?php
-// Headers required for CORS and Content-Type
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-include_once __DIR__ . '/../../config/Database.php';
-include_once __DIR__ . '/../../models/Category.php';
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
 
-
-// Instantiate DB & connect
 $database = new Database();
 $db = $database->connect();
 
-// Instantiate a Category object
 $category = new Category($db);
 
-// Get the request method
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
     case 'GET':
         if (isset($_GET['id'])) {
-            // Fetch a single category by id
-            include 'read_single.php';
+            $category->id = $_GET['id'];
+            $category->readSingle();
+            if (!empty($category->category)) {
+                echo json_encode([
+                    'id' => $category->id,
+                    'category' => $category->category
+                ]);
+            } else {
+                echo json_encode(['message' => 'category_id Not Found']);
+            }
         } else {
-            // Fetch all categories
-            include 'read.php';
+            $result = $category->read();
+            $num = $result->rowCount();
+
+            if ($num > 0) {
+                $categories_arr = [];
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                    extract($row);
+                    $categories_arr[] = [
+                        'id' => $id,
+                        'category' => $category
+                    ];
+                }
+                echo json_encode($categories_arr);
+            } else {
+                echo json_encode(['message' => 'No Categories Found']);
+            }
         }
         break;
+
     case 'POST':
-        // Create a new category
         include 'create.php';
         break;
+
     case 'PUT':
-        // Update an existing category
         include 'update.php';
         break;
+
     case 'DELETE':
-        // Delete a category
         include 'delete.php';
         break;
+
     default:
-        // Method Not Allowed
         http_response_code(405);
-        echo json_encode(array('message' => 'Method Not Allowed'));
+        echo json_encode(['message' => 'Method Not Allowed']);
         break;
 }
