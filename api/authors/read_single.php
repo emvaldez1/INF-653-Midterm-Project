dfa2dc1ba90a8ec236dfc74c1cffc24a5a16a103
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 13:06:34 -0500

    Update read_single.php

--- api/authors/read_single.php
+++ api/authors/read_single.php
@@ -5,23 +5,32 @@ header('Content-Type: application/json');
 include_once __DIR__ . '/../../config/Database.php';
 include_once __DIR__ . '/../../models/Author.php';
 
+// Instantiate DB & connect
 $database = new Database();
-$db = the database->connect();
+$db = $database->connect();
 
+// Instantiate an author object
 $author = new Author($db);
 
-$author->id = isset($_GET['id']) ? $_GET['id'] : die();
+// Get ID from URL
+$author->id = isset($_GET['id']) ? $_GET['id'] : null;
 
+// Get author
 $author->readSingle();
 
 if($author->author != null) {
-    http_response_code(200);
+    // Create array
     $author_arr = array(
         'id' => $author->id,
         'author' => $author->author
     );
-    echo json_encode($author_arr);
+
+    // Make JSON
+    print_r(json_encode($author_arr));
 } else {
+    // Set response code - 404 Not found
     http_response_code(404);
+
+    // Author not found
     echo json_encode(array('message' => 'Author Not Found'));
 }
