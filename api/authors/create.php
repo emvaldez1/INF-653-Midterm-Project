5ac46bc4f6224c3b50aab3359359845ebf645057
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 20:18:19 -0500

    Update create.php

--- api/authors/create.php
+++ api/authors/create.php
@@ -4,8 +4,8 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-include_once '../config/Database.php';
-include_once 'Author.php';
+include_once __DIR__ . '/../../config/Database.php';
+include_once __DIR__ . '/../../models/Author.php';
 
 $database = new Database();
 $db = $database->connect();
