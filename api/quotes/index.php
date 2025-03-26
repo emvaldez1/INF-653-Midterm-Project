ce9617195eef4c39ad9e10c774ec42e167a9795e
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 20:34:48 -0500

    Update index.php

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -5,8 +5,8 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-include_once __DIR__ . '/../../config/Database.php';
-include_once __DIR__ . '/../../models/Quote.php';
+include_once '../../config/Database.php';
+include_once '../../models/Quote.php';
 
 // Instantiate DB & connect
 $database = new Database();
@@ -21,29 +21,55 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
     case 'GET':
         if (isset($_GET['id'])) {
-            // Fetch a single quote by id
             include 'read_single.php';
+        } elseif (isset($_GET['author_id']) && isset($_GET['category_id'])) {
+            $quote->author_id = $_GET['author_id'];
+            $quote->category_id = $_GET['category_id'];
+            $result = $quote->readByAuthorAndCategory();
+        } elseif (isset($_GET['author_id'])) {
+            $quote->author_id = $_GET['author_id'];
+            $result = $quote->readByAuthor();
+        } elseif (isset($_GET['category_id'])) {
+            $quote->category_id = $_GET['category_id'];
+            $result = $quote->readByCategory();
         } else {
-            // Fetch all quotes
             include 'read.php';
+            break;
+        }
+
+        $num = $result->rowCount();
+        if ($num > 0) {
+            $quotes_arr = array();
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                extract($row);
+                $quote_item = array(
+                    'id' => $id,
+                    'quote' => $quote,
+                    'author' => $author,
+                    'category' => $category
+                );
+                array_push($quotes_arr, $quote_item);
+            }
+            echo json_encode($quotes_arr);
+        } else {
+            echo json_encode(array('message' => 'No Quotes Found'));
         }
         break;
+
     case 'POST':
-        // Create a new quote
         include 'create.php';
         break;
+
     case 'PUT':
-        // Update an existing quote
         include 'update.php';
         break;
+
     case 'DELETE':
-        // Delete a quote
         include 'delete.php';
         break;
+
     default:
-        // Method Not Allowed
         http_response_code(405);
         echo json_encode(array('message' => 'Method Not Allowed'));
         break;
 }
-
