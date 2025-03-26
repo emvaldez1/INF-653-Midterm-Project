4d5b2aed05f4c307397c3ce65dac20efb05cacfa
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 16:46:17 -0500

    Update delete.php

--- api/categories/delete.php
+++ api/categories/delete.php
@@ -1,30 +1,21 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: DELETE');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
-
-include_once '../../config/Database.php';
-include_once '../../models/Category.php';
-
-$database = new Database();
-$db = the database->connect();
-
-$category = new Category($db);
-
 $data = json_decode(file_get_contents("php://input"));
-
 if (!empty($data->id)) {
-    $category->id = $data->id;
-
-    if ($category->delete()) {
-        http_response_code(200);
-        echo json_encode(['id' => $category->id]);
-    } else {
+    $category->id = intval($data->id);
+    if (!$category->read_single()) {
         http_response_code(404);
-        echo json_encode(['message' => 'Category Not Found']);
+        echo json_encode(['message' => 'categoryId Not Found']);
+    } else {
+        if ($category->delete()) {
+            echo json_encode(['id' => $category->id]);
+        } else {
+            echo json_encode(['message' => 'Category Not Deleted']);
+            http_response_code(500);
+        }
     }
 } else {
-    http_response_code(400);
     echo json_encode(['message' => 'Missing Required Parameters']);
+    http_response_code(400);
 }
+exit();
+?>
