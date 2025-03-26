c1ec2aa3a0e98517f08750f1a86cba7b7d855d8f
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 09:47:20 -0500

    Update Category.php

--- models/Category.php
+++ models/Category.php
@@ -1,7 +1,7 @@
 <?php
 class Category {
     private $conn;
-    private $table_name = "categories";
+    private $table_name = 'categories';
 
     public $id;
     public $category;
@@ -11,45 +11,45 @@ class Category {
     }
 
     public function read() {
-        $query = "SELECT id, category FROM " . $this->table_name;
+        $query = 'SELECT id, category FROM ' . $this->table_name;
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         return $stmt;
     }
 
-    public function readSingle() {
-        $query = "SELECT id, category FROM " . $this->table_name . " WHERE id = ?";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(1, $this->id);
-        $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($row) {
-            $this->category = $row['category'];
-        }
-    }
-
     public function create() {
-        $query = "INSERT INTO " . $this->table_name . " SET category=:category";
+        $query = 'INSERT INTO ' . $this->table_name . ' (category) VALUES (:category)';
         $stmt = $this->conn->prepare($query);
         $this->category = htmlspecialchars(strip_tags($this->category));
-        $stmt->bindParam(":category", $this->category);
-        return $stmt->execute();
+        $stmt->bindParam(':category', $this->category);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 
     public function update() {
-        $query = "UPDATE " . $this->table_name . " SET category=:category WHERE id = :id";
+        $query = 'UPDATE ' . $this->table_name . ' SET category = :category WHERE id = :id';
         $stmt = $this->conn->prepare($query);
         $this->category = htmlspecialchars(strip_tags($this->category));
         $stmt->bindParam(':category', $this->category);
         $stmt->bindParam(':id', $this->id);
-        return $stmt->execute();
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 
     public function delete() {
-        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+        $query = 'DELETE FROM ' . $this->table_name . ' WHERE id = :id';
         $stmt = $this->conn->prepare($query);
-        $this->id = htmlspecialchars(strip_tags($this->id));
-        $stmt->bindParam(1, $this->id);
-        return $stmt->execute();
+        $stmt->bindParam(':id', $this->id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 }
