53b4274dd6276c6cf98eb7777cd75a9ce43614e0
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 20:38:19 -0500

    Update create.php

--- api/categories/create.php
+++ api/categories/create.php
@@ -4,9 +4,8 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-include_once __DIR__ . '/../../config/Database.php';
-include_once __DIR__ . '/../../models/Category.php';
-
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
 
 $database = new Database();
 $db = $database->connect();
@@ -18,11 +17,14 @@ $data = json_decode(file_get_contents("php://input"));
 if (!empty($data->category)) {
     $category->category = $data->category;
 
-    if($category->create()) {
-        echo json_encode(['message' => 'Category Created']);
+    if ($category->create()) {
+        echo json_encode([
+            'id' => $category->id,
+            'category' => $category->category
+        ]);
     } else {
         echo json_encode(['message' => 'Category Not Created']);
     }
 } else {
-    echo json_encode(['message' => 'Category Not Provided']);
+    echo json_encode(['message' => 'Missing Required Parameters']);
 }
