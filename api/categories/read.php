e9bb0fe946ab5f7af943f307ff35a28460126280
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 09:47:57 -0500

    Update read.php

--- api/categories/read.php
+++ api/categories/read.php
@@ -2,31 +2,28 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-include_once __DIR__ . '/../../config/Database.php';
-include_once __DIR__ . '/../../models/Category.php';
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
 
 $database = new Database();
 $db = $database->connect();
-
 $category = new Category($db);
 
 $result = $category->read();
 $num = $result->rowCount();
 
 if ($num > 0) {
-    $categories_arr = array();
-    $categories_arr['data'] = array();
-
+    $categories_arr = [];
     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
-        $category_item = array(
+        $category_item = [
             'id' => $id,
             'category' => $category
-        );
-        array_push($categories_arr['data'], $category_item);
+        ];
+        array_push($categories_arr, $category_item);
     }
-
     echo json_encode($categories_arr);
 } else {
-    echo json_encode(array('message' => 'No Categories Found'));
+    http_response_code(404);
+    echo json_encode(['message' => 'No Categories Found']);
 }
