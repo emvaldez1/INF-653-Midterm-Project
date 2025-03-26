a3c216631ef614907945a311b98e235fed6cff42
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 17:01:55 -0500

    Update create.php

--- api/authors/create.php
+++ api/authors/create.php
@@ -1,24 +1,35 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+include_once '../../config/Database.php';
+include_once '../../models/Author.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$author = new Author($db);
+
 $data = json_decode(file_get_contents("php://input"));
+
 if (!empty($data->author)) {
     $author->author = $data->author;
+
     if ($author->create()) {
-        // Creation successful, return the new record with its ID
-        $newId = $db->lastInsertId();  // works for PostgreSQL if sequence is set
         echo json_encode([
-            'id' => $newId,
+            'id' => $author->id,
             'author' => $author->author
         ]);
-        http_response_code(201);  // Created
+        http_response_code(201); // Created
     } else {
-        // In case insertion failed (unlikely here), return error
+        http_response_code(500); // Server error
         echo json_encode(['message' => 'Author Not Created']);
-        http_response_code(500);
     }
 } else {
-    // Missing 'author' field in input
+    http_response_code(400); // Bad request
     echo json_encode(['message' => 'Missing Required Parameters']);
-    http_response_code(400);
 }
 exit();
 ?>
