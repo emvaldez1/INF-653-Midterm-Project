2610bed6d4ea51e5c1db5809cedb2322cc75fd7d
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 09:50:03 -0500

    Update read.php

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -1,5 +1,4 @@
 <?php
-// api/quotes/read.php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
@@ -8,26 +7,25 @@ include_once '../../models/Quote.php';
 
 $database = new Database();
 $db = $database->connect();
-
 $quote = new Quote($db);
 
 $result = $quote->read();
 $num = $result->rowCount();
 
 if ($num > 0) {
-    $quotes_arr = array();
-
+    $quotes_arr = [];
     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        $quote_item = array(
-            'id' => $row['id'],
-            'quote' => $row['quote'],
-            'author' => $row['author'],
-            'category' => $row['category']
-        );
+        extract($row);
+        $quote_item = [
+            'id' => $id,
+            'quote' => $quote,
+            'author' => $author_name,
+            'category' => $category_name
+        ];
         array_push($quotes_arr, $quote_item);
     }
-
     echo json_encode($quotes_arr);
 } else {
+    http_response_code(404);
     echo json_encode(['message' => 'No Quotes Found']);
 }
