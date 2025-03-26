49ddb9f5abea45d6a6cae2d6fa47a31c8ea1ea8b
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 17:56:58 -0500

    Update Author.php

--- models/Author.php
+++ models/Author.php
@@ -1,18 +1,15 @@
 <?php
 class Author {
     private $conn;
-    private $table = 'authors';  // Database table name
+    private $table = 'authors';
 
-    // Table columns as public properties
     public $id;
     public $author;
 
-    // Constructor with DB connection
     public function __construct($db) {
         $this->conn = $db;
     }
 
-    // READ all authors
     public function read() {
         $query = "SELECT * FROM {$this->table} ORDER BY id";
         $stmt = $this->conn->prepare($query);
@@ -20,7 +17,6 @@ class Author {
         return $stmt;
     }
 
-    // READ single author by id
     public function read_single() {
         $query = "SELECT id, author FROM {$this->table} WHERE id = :id";
         $stmt = $this->conn->prepare($query);
@@ -28,41 +24,32 @@ class Author {
         $stmt->execute();
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         if ($row) {
-            // Populate object properties
             $this->author = $row['author'];
-            return true;  // Data found
+            return true;
         }
-        return false;  // Not found
+        return false;
     }
 
-    // CREATE new author
     public function create() {
         $query = "INSERT INTO {$this->table} (author) VALUES (:author)";
         $stmt = $this->conn->prepare($query);
-        // Clean input
         $this->author = htmlspecialchars(strip_tags($this->author));
         $stmt->bindParam(':author', $this->author);
-        // Execute and return status
         return $stmt->execute();
     }
 
-    // UPDATE author by id
     public function update() {
         $query = "UPDATE {$this->table} SET author = :author WHERE id = :id";
         $stmt = $this->conn->prepare($query);
-        // Clean input
-        $this->id = htmlspecialchars(strip_tags($this->id));
         $this->author = htmlspecialchars(strip_tags($this->author));
         $stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
         $stmt->bindParam(':author', $this->author);
         return $stmt->execute();
     }
 
-    // DELETE author by id
     public function delete() {
         $query = "DELETE FROM {$this->table} WHERE id = :id";
         $stmt = $this->conn->prepare($query);
-        $this->id = htmlspecialchars(strip_tags($this->id));
         $stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
         return $stmt->execute();
     }
