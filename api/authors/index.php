a9e511307dfd5c45bbe042e51e003b2f14dffb53
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 20:17:08 -0500

    Update index.php

--- api/authors/index.php
+++ api/authors/index.php
@@ -5,8 +5,8 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-include_once '../config/Database.php';
-include_once 'Author.php';
+include_once __DIR__ . '/../../config/Database.php';
+include_once __DIR__ . '/../../models/Author.php';
 
 // Instantiate DB & connect
 $database = new Database();
