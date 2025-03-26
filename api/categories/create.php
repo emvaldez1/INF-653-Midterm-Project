c6416bfff3886c2fc56dd84f1f0ef39e496f1f83
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 20:41:02 -0500

    Update create.php

--- api/categories/create.php
+++ api/categories/create.php
@@ -5,25 +5,29 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once '../../config/Database.php';
-include_once '../../models/Category.php';
+include_once '../../models/Quote.php';
 
 $database = new Database();
 $db = $database->connect();
 
-$category = new Category($db);
+$quote = new Quote($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
-if (!empty($data->category)) {
-    $category->category = $data->category;
+if (!empty($data->quote) && !empty($data->author_id) && !empty($data->category_id)) {
+    $quote->quote = $data->quote;
+    $quote->author_id = $data->author_id;
+    $quote->category_id = $data->category_id;
 
-    if ($category->create()) {
+    if ($quote->create()) {
         echo json_encode([
-            'id' => $category->id,
-            'category' => $category->category
+            'id' => $quote->id,
+            'quote' => $quote->quote,
+            'author_id' => $quote->author_id,
+            'category_id' => $quote->category_id
         ]);
     } else {
-        echo json_encode(['message' => 'Category Not Created']);
+        echo json_encode(['message' => 'Quote Not Created']);
     }
 } else {
     echo json_encode(['message' => 'Missing Required Parameters']);
