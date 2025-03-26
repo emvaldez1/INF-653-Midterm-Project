fb92d40a5c9c02aad0f10a9ff86fa36fdcf6dd83
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 17:02:41 -0500

    Update delete.php

--- api/authors/delete.php
+++ api/authors/delete.php
@@ -1,23 +1,31 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+include_once '../../config/Database.php';
+include_once '../../models/Author.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$author = new Author($db);
+
 $data = json_decode(file_get_contents("php://input"));
+
 if (!empty($data->id)) {
-    $author->id = intval($data->id);
-    // Verify the author exists
-    if (!$author->read_single()) {
-        http_response_code(404);
-        echo json_encode(['message' => 'authorId Not Found']);
+    $author->id = $data->id;
+
+    if ($author->delete()) {
+        echo json_encode(['id' => $author->id]);
     } else {
-        // Try to delete
-        if ($author->delete()) {
-            echo json_encode(['id' => $author->id]);
-        } else {
-            echo json_encode(['message' => 'Author Not Deleted']);
-            http_response_code(500);
-        }
+        http_response_code(500); // Server error
+        echo json_encode(['message' => 'Author Not Deleted']);
     }
 } else {
+    http_response_code(400); // Bad request
     echo json_encode(['message' => 'Missing Required Parameters']);
-    http_response_code(400);
 }
 exit();
 ?>
