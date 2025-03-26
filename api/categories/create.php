7827e9657e555b7c28881bb7c45eec5113dc3d8c
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 17:00:35 -0500

    Update create.php

--- api/categories/create.php
+++ api/categories/create.php
@@ -1,21 +1,35 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$category = new Category($db);
+
 $data = json_decode(file_get_contents("php://input"));
+
 if (!empty($data->category)) {
     $category->category = $data->category;
+
     if ($category->create()) {
-        $newId = $db->lastInsertId();
         echo json_encode([
-            'id' => $newId,
+            'id' => $category->id,
             'category' => $category->category
         ]);
-        http_response_code(201);
+        http_response_code(201); // Created
     } else {
+        http_response_code(500); // Server error
         echo json_encode(['message' => 'Category Not Created']);
-        http_response_code(500);
     }
 } else {
+    http_response_code(400); // Bad request
     echo json_encode(['message' => 'Missing Required Parameters']);
-    http_response_code(400);
 }
 exit();
 ?>
