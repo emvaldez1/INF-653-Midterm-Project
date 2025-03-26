eb1b06dd498db9729b28c6ba5dc06693ebcc1e3c
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 20:18:49 -0500

    Update read_single.php

--- api/authors/read_single.php
+++ api/authors/read_single.php
@@ -3,8 +3,8 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-include_once '../config/Database.php';
-include_once 'Author.php';
+include_once __DIR__ . '/../../config/Database.php';
+include_once __DIR__ . '/../../models/Author.php';
 
 // Instantiate DB & connect
 $database = new Database();
