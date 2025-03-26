fdc0e2b0aad9c6232d9373626c33d3dae61e53a1
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 13:12:19 -0500

    Update create.php

--- api/authors/create.php
+++ api/authors/create.php
@@ -5,30 +5,26 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once '../../config/Database.php';
-include_once '../../models/Quote.php';
+include_once '../../models/Author.php';
 
 $database = new Database();
 $db = $database->connect();
 
-$quote = new Quote($db);
+$author = new Author($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
-if (!empty($data->quote) && !empty($data->author_id) && !empty($data->category_id)) {
-    $quote->quote = $data->quote;
-    $quote->author_id = $data->author_id;
-    $quote->category_id = $data->category_id;
+if (!empty($data->author)) {
+    $author->author = $data->author;
 
-    if ($quote->create()) {
+    if ($author->create()) {
         echo json_encode([
-            'id' => $quote->id,
-            'quote' => $quote->quote,
-            'author_id' => $quote->author_id,
-            'category_id' => $quote->category_id
+            'id' => $author->id,
+            'author' => $author->author
         ]);
     } else {
-        http_response_code(404);
-        echo json_encode(['message' => 'Author or Category Not Found']);
+        http_response_code(500);
+        echo json_encode(['message' => 'Author Not Created']);
     }
 } else {
     http_response_code(400);
