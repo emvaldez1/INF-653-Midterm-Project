4ac0c91b4c1b84c2ad45432a3004be18d89efdc6
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 10:47:04 -0500

    Update create.php

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -1,4 +1,8 @@
 <?php
+
+ini_set('display_errors', 0);
+error_reporting(E_ALL);
+
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
