38e24152b1e66fb3ed696b5f46bfecd1f0705a79
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 10:17:54 -0500

    Update create.php

--- api/authors/create.php
+++ api/authors/create.php
@@ -10,28 +10,21 @@ include_once '../../models/Author.php';
 $database = new Database();
 $db = $database->connect();
 
-if (!$db) {
-    http_response_code(500); // Internal Server Error
-    echo json_encode(['message' => 'Database connection failed']);
-    exit;
-}
-
 $author = new Author($db);
+
 $data = json_decode(file_get_contents("php://input"));
 
 if (!empty($data->author)) {
     $author->author = $data->author;
+
     if ($author->create()) {
-        http_response_code(201);
         echo json_encode([
-            'id' => $db->lastInsertId(),
+            'id' => $author->id,
             'author' => $author->author
         ]);
     } else {
-        http_response_code(503); // Service Unavailable
         echo json_encode(['message' => 'Author Not Created']);
     }
 } else {
-    http_response_code(400); // Bad Request
     echo json_encode(['message' => 'Missing Required Parameters']);
 }
