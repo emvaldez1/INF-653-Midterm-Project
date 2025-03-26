1e514941126bd7524bb37e83ce1c76f4527d1d62
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 17:57:12 -0500

    Update Category.php

--- models/Category.php
+++ models/Category.php
@@ -10,7 +10,6 @@ class Category {
         $this->conn = $db;
     }
 
-    // READ all categories
     public function read() {
         $query = "SELECT * FROM {$this->table} ORDER BY id";
         $stmt = $this->conn->prepare($query);
@@ -18,7 +17,6 @@ class Category {
         return $stmt;
     }
 
-    // READ single category by id
     public function read_single() {
         $query = "SELECT id, category FROM {$this->table} WHERE id = :id";
         $stmt = $this->conn->prepare($query);
@@ -32,7 +30,6 @@ class Category {
         return false;
     }
 
-    // CREATE new category
     public function create() {
         $query = "INSERT INTO {$this->table} (category) VALUES (:category)";
         $stmt = $this->conn->prepare($query);
@@ -41,22 +38,18 @@ class Category {
         return $stmt->execute();
     }
 
-    // UPDATE category by id
     public function update() {
         $query = "UPDATE {$this->table} SET category = :category WHERE id = :id";
         $stmt = $this->conn->prepare($query);
-        $this->id = htmlspecialchars(strip_tags($this->id));
         $this->category = htmlspecialchars(strip_tags($this->category));
         $stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
         $stmt->bindParam(':category', $this->category);
         return $stmt->execute();
     }
 
-    // DELETE category by id
     public function delete() {
         $query = "DELETE FROM {$this->table} WHERE id = :id";
         $stmt = $this->conn->prepare($query);
-        $this->id = htmlspecialchars(strip_tags($this->id));
         $stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
         return $stmt->execute();
     }
