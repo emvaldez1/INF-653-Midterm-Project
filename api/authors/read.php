47be77a56866e6595ea90450b0374ed0018b51ba
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 10:18:45 -0500

    Update read.php

--- api/authors/read.php
+++ api/authors/read.php
@@ -2,28 +2,24 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-include_once '../../config/Database.php';
-include_once '../../models/Author.php';
+include_once __DIR__ . '/../../config/Database.php';
+include_once __DIR__ . '/../../models/Author.php';
 
 $database = new Database();
 $db = $database->connect();
 $author = new Author($db);
-
 $result = $author->read();
 $num = $result->rowCount();
 
 if ($num > 0) {
-    $authors_arr = [];
+    $authors_arr = array();
+    $authors_arr['data'] = array();
     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
-        $author_item = [
-            'id' => $id,
-            'author' => $author
-        ];
-        array_push($authors_arr, $author_item);
+        $author_item = array('id' => $id, 'author' => $author);
+        array_push($authors_arr['data'], $author_item);
     }
     echo json_encode($authors_arr);
 } else {
-    http_response_code(404);
-    echo json_encode(['message' => 'No Authors Found']);
+    echo json_encode(array('message' => 'No Authors Found'));
 }
