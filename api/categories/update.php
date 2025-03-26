38c056181d055d25049ed20b91fb29d032f77c2b
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 17:01:06 -0500

    Update update.php

--- api/categories/update.php
+++ api/categories/update.php
@@ -1,25 +1,35 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$category = new Category($db);
+
 $data = json_decode(file_get_contents("php://input"));
+
 if (!empty($data->id) && !empty($data->category)) {
-    $category->id = intval($data->id);
+    $category->id = $data->id;
     $category->category = $data->category;
-    if (!$category->read_single()) {
-        http_response_code(404);
-        echo json_encode(['message' => 'categoryId Not Found']);
+
+    if ($category->update()) {
+        echo json_encode([
+            'id' => $category->id,
+            'category' => $category->category
+        ]);
     } else {
-        if ($category->update()) {
-            echo json_encode([
-                'id' => $category->id,
-                'category' => $category->category
-            ]);
-        } else {
-            echo json_encode(['message' => 'Category Not Updated']);
-            http_response_code(500);
-        }
+        http_response_code(500); // Server error
+        echo json_encode(['message' => 'Category Not Updated']);
     }
 } else {
+    http_response_code(400); // Bad request
     echo json_encode(['message' => 'Missing Required Parameters']);
-    http_response_code(400);
 }
 exit();
 ?>
