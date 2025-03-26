5e58998116cc26e7e24395eb952cd399694a9ca6
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 10:01:52 -0500

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -9,7 +9,7 @@ class Database {
     public function connect() {
         $this->conn = null;
         try {
-            $dsn = 'pgsql:host=' . $this->host . ';port=5432;dbname=' . $this->db_name; // Make sure the port is correct, usually 5432 for PostgreSQL
+            $dsn = 'pgsql:host=' . $this->host . ';port=5432;dbname=' . $this->db_name;
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
