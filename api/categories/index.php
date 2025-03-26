41e372dad17c66bf149165049ef34cc7e4227e3a
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 20:17:25 -0500

    Update index.php

--- api/categories/index.php
+++ api/categories/index.php
@@ -5,8 +5,9 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-include_once '../config/Database.php';
-include_once 'Category.php'; // Assuming you have a Category class to handle the database operations
+include_once __DIR__ . '/../../config/Database.php';
+include_once __DIR__ . '/../../models/Category.php';
+
 
 // Instantiate DB & connect
 $database = new Database();
