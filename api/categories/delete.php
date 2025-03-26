a91a8e2da882fd55b005ba9618bffdc39caadeb9
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 20:39:49 -0500

    Update delete.php

--- api/categories/delete.php
+++ api/categories/delete.php
@@ -4,9 +4,8 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-include_once __DIR__ . '/../../config/Database.php';
-include_once __DIR__ . '/../../models/Category.php';
-
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
 
 $database = new Database();
 $db = $database->connect();
@@ -18,11 +17,11 @@ $data = json_decode(file_get_contents("php://input"));
 if (!empty($data->id)) {
     $category->id = $data->id;
 
-    if($category->delete()) {
-        echo json_encode(['message' => 'Category Deleted']);
+    if ($category->delete()) {
+        echo json_encode(['id' => $category->id]);
     } else {
-        echo json_encode(['message' => 'Category Not Deleted']);
+        echo json_encode(['message' => 'No Categories Found']);
     }
 } else {
-    echo json_encode(['message' => 'Category ID Not Provided']);
+    echo json_encode(['message' => 'Missing Required Parameters']);
 }
