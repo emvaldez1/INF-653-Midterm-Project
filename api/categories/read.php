fc77fd8dc5f79e25304b67d5eb88bb9f18cc947f
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 17:00:52 -0500

    Update read.php

--- api/categories/read.php
+++ api/categories/read.php
@@ -1,28 +1,49 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$category = new Category($db);
+
 if (isset($_GET['id'])) {
-    $category->id = intval($_GET['id']);
-    if ($category->read_single()) {
+    $category->id = $_GET['id'];
+
+    if($category->readSingle()) {
         echo json_encode([
             'id' => $category->id,
             'category' => $category->category
         ]);
     } else {
-        http_response_code(404);
-        echo json_encode(['message' => 'categoryId Not Found']);
+        http_response_code(404); // Not Found
+        echo json_encode(['message' => 'Category Not Found']);
     }
 } else {
     $result = $category->read();
-    $rowCount = $result->rowCount();
-    if ($rowCount > 0) {
-        $categoriesArr = [];
+    $num = $result->rowCount();
+
+    if ($num > 0) {
+        $categories_arr = [];
+
         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            $categoriesArr[] = [
-                'id' => $row['id'],
-                'category' => $row['category']
+            extract($row);
+            $category_item = [
+                'id' => $id,
+                'category' => $category
             ];
+
+            array_push($categories_arr, $category_item);
         }
-        echo json_encode($categoriesArr);
+
+        echo json_encode($categories_arr);
     } else {
+        http_response_code(404); // Not Found
         echo json_encode(['message' => 'No Categories Found']);
     }
 }
