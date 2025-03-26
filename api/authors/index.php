4f06fd13975bd50f34c62c802dc8c432ee26eb70
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 12:30:01 -0500

    Update index.php

--- api/authors/index.php
+++ api/authors/index.php
@@ -1,9 +1,4 @@
 <?php
-
-ini_set('display_errors', 0); // Turn off displaying errors
-error_reporting(E_ALL); // Report all errors internally
-
-
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
@@ -22,83 +17,59 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
     case 'GET':
         $author->id = isset($_GET['id']) ? $_GET['id'] : null;
-
-        if ($author->id) {
-            $author->readSingle();
-            if ($author->author) {
-                echo json_encode([
-                    'id' => $author->id,
-                    'author' => $author->author
-                ]);
-            } else {
-                echo json_encode(['message' => 'author_id Not Found']);
-            }
-        } else {
-            $result = $author->read();
-            $num = $result->rowCount();
-
-            if ($num > 0) {
-                $authors_arr = [];
-
-                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    extract($row);
-                    $authors_arr[] = [
-                        'id' => $id,
-                        'author' => $author
-                    ];
-                }
-                echo json_encode($authors_arr);
-            } else {
-                echo json_encode(['message' => 'No Authors Found']);
-            }
-        }
+        $response = $author->id ? $author->readSingle() : $author->read();
+        http_response_code(200);
+        echo json_encode($response);
         break;
 
     case 'POST':
         $data = json_decode(file_get_contents("php://input"));
-
         if (!empty($data->author)) {
             $author->author = $data->author;
-
             if ($author->create()) {
+                http_response_code(201);
                 echo json_encode(['id' => $author->id, 'author' => $author->author]);
             } else {
+                http_response_code(500);
                 echo json_encode(['message' => 'Author Not Created']);
             }
         } else {
+            http_response_code(400);
             echo json_encode(['message' => 'Missing Required Parameters']);
         }
         break;
 
     case 'PUT':
         $data = json_decode(file_get_contents("php://input"));
-
         if (!empty($data->id) && !empty($data->author)) {
             $author->id = $data->id;
             $author->author = $data->author;
-
             if ($author->update()) {
+                http_response_code(200);
                 echo json_encode(['id' => $author->id, 'author' => $author->author]);
             } else {
-                echo json_encode(['message' => 'Author Not Updated']);
+                http_response_code(404);
+                echo json_encode(['message' => 'Author Not Found']);
             }
         } else {
+            http_response_code(400);
             echo json_encode(['message' => 'Missing Required Parameters']);
         }
         break;
 
     case 'DELETE':
         $data = json_decode(file_get_contents("php://input"));
-
         if (!empty($data->id)) {
             $author->id = $data->id;
-
             if ($author->delete()) {
+                http_response_code(200);
                 echo json_encode(['id' => $author->id]);
             } else {
-                echo json_encode(['message' => 'No Authors Found']);
+                http_response_code(404);
+                echo json_encode(['message' => 'Author Not Found']);
             }
         } else {
+            http_response_code(400);
             echo json_encode(['message' => 'Missing Required Parameters']);
         }
         break;
