74d5fe5f63433392a374e07ff905480c4581f823
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 21:07:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
 <?php
-// Example structure for `index.php` in `/api`
 header('Content-Type: application/json');
 
-$requestUri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
-$apiIndex = array_search('api', $requestUri);
+$request = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+$api_index = array_search('api', $request);
 
-if (isset($requestUri[$apiIndex + 1])) {
-    $resource = $requestUri[$apiIndex + 1];
-    switch ($resource) {
-        case 'authors':
-            require 'authors/index.php';
-            break;
-        case 'categories':
-            require 'categories/index.php';
-            break;
-        case 'quotes':
-            require 'quotes/index.php';
-            break;
-        default:
-            http_response_code(404);
-            echo json_encode(['message' => 'Resource not found']);
-            break;
-    }
-} else {
-    echo json_encode(['message' => 'No resource specified']);
+if ($api_index === false || !isset($request[$api_index + 1])) {
+    echo json_encode(['message' => 'API endpoint not found']);
+    exit;
+}
+
+switch ($request[$api_index + 1]) {
+    case 'quotes':
+        require 'api/quotes/index.php';
+        break;
+    case 'authors':
+        require 'api/authors/index.php';
+        break;
+    case 'categories':
+        require 'api/categories/index.php';
+        break;
+    default:
+        echo json_encode(['message' => 'API endpoint not found']);
+        break;
 }
