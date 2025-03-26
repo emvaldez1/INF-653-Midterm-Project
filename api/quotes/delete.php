9c5d5df9f5f24407963627e9eb97d88e765809d6
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 16:46:34 -0500

    Update delete.php

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -1,30 +1,21 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: DELETE');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
-
-include_once '../../config/Database.php';
-include_once '../../models/Quote.php';
-
-$database = new Database();
-$db = $database->connect();
-
-$quote = new Quote($db);
-
 $data = json_decode(file_get_contents("php://input"));
-
 if (!empty($data->id)) {
-    $quote->id = $data->id;
-
-    if ($quote->delete()) {
-        http_response_code(200); // OK
-        echo json_encode(['id' => $quote->id]);
+    $quote->id = intval($data->id);
+    if (!$quote->read_single()) {
+        http_response_code(404);
+        echo json_encode(['message' => 'No Quotes Found']);
     } else {
-        http_response_code(404); // Not Found
-        echo json_encode(['message' => 'Quote Not Found']);
+        if ($quote->delete()) {
+            echo json_encode(['id' => $quote->id]);
+        } else {
+            echo json_encode(['message' => 'Quote Not Deleted']);
+            http_response_code(500);
+        }
     }
 } else {
-    http_response_code(400); // Bad Request
     echo json_encode(['message' => 'Missing Required Parameters']);
+    http_response_code(400);
 }
+exit();
+?>
