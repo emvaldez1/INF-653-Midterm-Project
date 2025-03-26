b87e9ab8016e0ae69f2e102a3d11c3026b0e53ae
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-26 12:51:13 -0500

    Update read_single.php

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -1,37 +1,29 @@
 <?php
-
-ini_set('display_errors', 0);
-error_reporting(E_ALL);
-
-// Set headers for CORS and content type
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 include_once '../../config/Database.php';
 include_once '../../models/Quote.php';
 
-// Instantiate DB and connect
 $database = new Database();
-$db = $database->connect();
+$db = the database->connect();
 
-// Instantiate quote object
 $quote = new Quote($db);
 
-// Check for ID in URL and assign to object
 $quote->id = isset($_GET['id']) ? $_GET['id'] : die();
 
-// Read quote
 $quote->readSingle();
 
 if ($quote->quote != null) {
+    http_response_code(200);
     $quote_arr = array(
         'id' => $quote->id,
         'quote' => $quote->quote,
         'author' => $quote->author_id,
         'category' => $quote->category_id
     );
-
     echo json_encode($quote_arr);
 } else {
+    http_response_code(404);
     echo json_encode(['message' => 'No Quotes Found']);
 }
