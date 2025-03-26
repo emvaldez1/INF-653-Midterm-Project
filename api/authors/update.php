51f60c88d799b07074c60f032204d063428f26dc
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-25 20:37:09 -0500

    Update update.php

--- api/authors/update.php
+++ api/authors/update.php
@@ -4,18 +4,28 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: PUT');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-include_once __DIR__ . '/../../config/Database.php';
-include_once __DIR__ . '/../../models/Author.php';
+include_once '../../config/Database.php';
+include_once '../../models/Author.php';
 
 $database = new Database();
 $db = $database->connect();
-$author = new Author($db);
-$data = json_decode(file_get_contents("php://input"));
-$author->id = $data->id;
-$author->author = $data->author;
 
-if($author->update()) {
-    echo json_encode(array('message' => 'Author Updated'));
+$author = new Author($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!empty($data->id) && !empty($data->author)) {
+    $author->id = $data->id;
+    $author->author = $data->author;
+
+    if ($author->update()) {
+        echo json_encode([
+            'id' => $author->id,
+            'author' => $author->author
+        ]);
+    } else {
+        echo json_encode(['message' => 'No Authors Found']);
+    }
 } else {
-    echo json_encode(array('message' => 'Author Not Updated'));
+    echo json_encode(['message' => 'Missing Required Parameters']);
 }
