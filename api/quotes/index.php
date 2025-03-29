f5f0588ffb42f02320d35ffefcb21e2bcc6a8e43
Elise Valdez <156866342+emvaldez1@users.noreply.github.com>
2025-03-29 16:07:18 -0500

    Update index.php

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,5 +1,4 @@
 <?php
-// CORS and content-type headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 $method = $_SERVER['REQUEST_METHOD'];
@@ -13,23 +12,17 @@ if ($method === 'OPTIONS') {
 include_once '../../config/Database.php';
 include_once '../../models/Quote.php';
 
-// Instantiate DB & connect
 $database = new Database();
 $db = $database->connect();
 
-// Instantiate quote object
 $quote = new Quote($db);
 
-// GET parameters
 $id = isset($_GET['id']) ? $_GET['id'] : null;
 $author_id = isset($_GET['author_id']) ? $_GET['author_id'] : null;
 $category_id = isset($_GET['category_id']) ? $_GET['category_id'] : null;
-$random = isset($_GET['random']) ? $_GET['random'] : null;
 
-// Single Quote by ID
 if ($id) {
-    $quote->id = $id;
-    if ($quote->read_single()) {
+    if ($quote->read_single($id)) {
         $quote_arr = array(
             'id' => $quote->id,
             'quote' => $quote->quote,
@@ -41,31 +34,20 @@ if ($id) {
         http_response_code(404);
         echo json_encode(['message' => 'No Quotes Found']);
     }
-
-// Filter by author_id, category_id, and/or random
 } else {
-    $result = $quote->read_filtered($author_id, $category_id, $random);
-    $num = $result->rowCount();
+    $result = $quote->read_filtered($author_id, $category_id);
+    $quotes_arr = array();
 
-    if ($num > 0) {
-        $quotes_arr = array();
-        $quotes_arr['data'] = array();
-
-        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            extract($row);
-            $quote_item = array(
-                'id' => $id,
-                'quote' => $quote,
-                'author' => $author,
-                'category' => $category
-            );
-            array_push($quotes_arr['data'], $quote_item);
-        }
-
-        echo json_encode($quotes_arr);
-    } else {
-        http_response_code(404);
-        echo json_encode(['message' => 'No Quotes Found']);
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $quote_item = array(
+            'id' => $id,
+            'quote' => $quote,
+            'author' => $author,
+            'category' => $category
+        );
+        array_push($quotes_arr, $quote_item);
     }
+    echo json_encode($quotes_arr);
 }
 ?>
